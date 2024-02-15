3df4a925aa5563399cc2c4ce626efc6bf4fd9bf0
Saroj Ghalan <jmestmnag@gmail.com>
2024-02-15 07:52:13 +0545

    observing through handle event

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import React from "react";
 import { Button, Switch, FormControlLabel } from "@material-ui/core";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import observable from "./Observable";
 function App() {
-  return <div>Observer pattern</div>;
+  function handleClick() {
+    observable.notify("User clicked button!");
+  }
+
+  function handleToggle() {
+    observable.notify("User toggled switch!");
+  }
+
+  function logger(data: any) {
+    console.log(`${Date.now()} ${data}`);
+  }
+
+  function toastify(data: any) {
+    toast(data);
+  }
+
+  observable.subscribe(logger);
+  observable.subscribe(toastify);
+  return (
+    <div className="App">
+      <Button variant="contained" onClick={handleClick}>
+        Click me!
+      </Button>
+      <FormControlLabel
+        control={<Switch name="" onChange={handleToggle} />}
+        label="Toggle me!"
+      />
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
